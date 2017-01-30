fb7b432b6882871b7078c7a53d68eccdd02ad5a4
Julian Jupiter <julianjupiter.io@gmail.com>
2017-01-30 21:39:45 +0800

    Create index.php

---
+++ view/home/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Home</title>
+	<head>
+	<body>
+		<h2>Home Page</h2>
+		<p>
+			<a href="/index.php">Home</a> | 
+			<a href="/index.php?page=phonebook">Phonebook</a> | 			
+			<a href="/index.php?page=auth&action=login">Login</a>
+		</p>
+	</body>	
+</html>
